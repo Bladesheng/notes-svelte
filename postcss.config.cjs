639c9444c2ad9c3072137a12f403ee3287f727b2
Bladesheng <keadr23@gmail.com>
2023-07-03 18:31:40 +0200

    Fix postcss @layers not working correctly

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -7,6 +7,12 @@ const config = {
       stage: 2,
       features: {
         "nesting-rules": true,
+
+        // For some unknown reason, "cascade-layers" polyfill doesn't get applied to svelte component css,
+        // (even though other polyfills like "nesting-rules" do get applied just fine???)
+        // which means that css from svelte components will always have the lowest specificity possible.
+        // Besides, css @layer has 90% global browser support anyway, so let's just use it without the polyfill.
+        "cascade-layers": false,
       },
     }),
   ],
